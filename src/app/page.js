4ebd985c45e376d54467f9848aa798c97680edf9
ashatrealmoneyleads <ash@realmoneyleads.com>
2023-06-01 19:11:44 +1000

    committing to new flask app

--- src/app/page.js
+++ src/app/page.js
@@ -61,7 +61,7 @@ export default function Home() {
           <h2>
             Learn <span>-&gt;</span>
           </h2>
-          <p>Just be patient and we are gonna get this... Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>Learning to set this up step by step... Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
 
         <a
